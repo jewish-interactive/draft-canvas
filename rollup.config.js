0a6ddd22a2e656e532c202357d66fd46001b6db0
jpuri <jyotipuri@gmail.com>
2018-01-27 23:31:11 +0530

    Config and build changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,26 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import replace from 'rollup-plugin-replace';
-//import uglify from 'rollup-plugin-uglify';
-//import { minify } from 'uglify-es';
+import postcss from 'rollup-plugin-postcss';
 
 export default [
 	{
         input: './src/lib/Lib.ts',
-        external: ['S','R','sanctuary','ramda', 'gl-matrix', 'react', 'react-dom', 'draft-js'],
+        external: ['react', 'react-dom', 'draft-js'],
         output: [
             { file: pkg.module, format: 'es', sourcemap: true },
             { file: pkg.main, format: 'cjs', sourcemap: true }
         ],
         plugins: [
+            postcss({
+                extensions: [ '.css' ],
+            }),
             replace({
                 'process.env.NODE_ENV': JSON.stringify( process.env['NODE_ENV'] )
             }),
-            
             typescript({
                 useTsconfigDeclarationDir: true
             }),
-            
-           // uglify({}, minify)
         ]
 	}
 ];
