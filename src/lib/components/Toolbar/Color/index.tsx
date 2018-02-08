4ae6660fadb06d091e7a7055742db5bf4835e307
jpuri <jyotipuri@gmail.com>
2018-02-09 00:08:10 +0530

    Adding color selector

--- src/lib/components/Toolbar/Color/index.tsx
+++ src/lib/components/Toolbar/Color/index.tsx
@@ -7,6 +7,16 @@ import {
 } from 'draft-js';
 import Color from '../../../../icons/color';
 import * as DraftJSUtils from 'draftjs-utils';
+import './styles.css';
+
+const classNames = require('classnames');
+
+const colors = ['rgb(97,189,109)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)',
+  'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)', 'rgb(0,168,133)',
+  'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)', 'rgb(40,50,78)', 'rgb(0,0,0)',
+  'rgb(247,218,100)', 'rgb(251,160,38)', 'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)',
+  'rgb(239,239,239)', 'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
+  'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)'];
 
 export interface Props {
   editorState: EditorState;
@@ -14,12 +24,14 @@ export interface Props {
 }
 
 export interface State {
-  currentTextColor?: string;
+  currentTextColor: string;
+  showSelector: boolean;
 }
 
 export default class FontFamily extends Component<Props, State> {
   state = {
     currentTextColor: '',
+    showSelector: false,
   }
 
   componentWillMount(): void {
@@ -31,6 +43,10 @@ export default class FontFamily extends Component<Props, State> {
           ['FONTFAMILY']
         ).FONTFAMILY,
     });
+  }
+
+  toggleShowSelector = () => {
+    this.setState({ showSelector: !this.state.showSelector });
   }
 
   onChange = (textColor) => {
@@ -49,9 +65,47 @@ export default class FontFamily extends Component<Props, State> {
   };
 
   render() {
-    const { currentTextColor } = this.state;
+    const { currentTextColor, showSelector } = this.state;
+    let currentStyle = 'color';
     return (
-      <button className="dce-toolbar-icon"><Color /></button>
+      <div className="dce-colorpicker-wrapper">
+        <button className="dce-toolbar-icon" onClick={this.toggleShowSelector}>
+          <Color />
+        </button>
+        {showSelector &&
+          <div className="dce-colorpicker-modal">
+            <span className="rdw-colorpicker-modal-header">
+              <span
+                className={classNames(
+                  'rdw-colorpicker-modal-style-label',
+                  { 'rdw-colorpicker-modal-style-label-active': currentStyle === 'color' },
+                )}
+              >
+                Text
+              </span>
+              <span
+                className={classNames(
+                  'rdw-colorpicker-modal-style-label',
+                  { 'rdw-colorpicker-modal-style-label-active': currentStyle === 'bgcolor' },
+                )}
+              >
+                Highlight
+              </span>
+            </span>
+            <span className="dce-colorpicker-modal-options">
+              {
+                colors.map((c, index) =>
+                  (<div className="dce-colorpicker-option">
+                    <div
+                      style={{ backgroundColor: c }}
+                      className="dce-colorpicker-cube"
+                    />
+                  </div>))
+              }
+            </span>
+          </div>
+        }
+      </div>
     );
   }
 }
