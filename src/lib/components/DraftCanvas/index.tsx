91f5f83e0770a6bda134002a60a34f7f331292fe
jpuri <jyotipuri@gmail.com>
2018-02-18 00:26:56 +0530

    Cropping canvas

--- src/lib/components/DraftCanvas/index.tsx
+++ src/lib/components/DraftCanvas/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { Component } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import { DraftEditor } from "../DraftEditor";
-import { Canvas } from "../Canvas";
+import { plotCanvas } from "../../utils/canvas";
 import "./styles.css";
 
 export interface Props {
@@ -14,28 +15,51 @@ export interface Props {
 
 export interface State {
   editorState: EditorState;
+  editorDimensions: any;
 }
 
 /**
  * Root component with DraftJS editor and Canvas components as children.
  */
 export class DraftCanvas extends Component<Props, State> {
+  canvas = undefined;
   state = {
-    editorState: EditorState.createEmpty()
+    editorState: EditorState.createEmpty(),
+    editorDimensions: { height: 500, width: 500 }
+  };
+
+  plotCanvas = editorState => {
+    const editorDimensions = plotCanvas(
+      this.props.target,
+      editorState,
+      500,
+      500
+    ) as any;
   };
 
   onChange = editorState => {
-    this.setState({ editorState });
+    const editorDimensions = this.plotCanvas(editorState);
+    this.setState({ editorState, editorDimensions });
   };
 
   onSave = () => {
-    const { editorState } = this.state;
+    const { editorState, editorDimensions } = this.state;
     const rawDraftContentState = convertToRaw(editorState.getCurrentContent());
+    const { width, height } = editorDimensions;
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas
+      .getContext("2d")
+      .drawImage(this.props.target, 0, 0, width, height, 0, 0, width, height);
     this.props.onSave({ rawDraftContentState });
   };
 
+  getCanvasRef = ref => {
+    this.canvas = ReactDOM.findDOMNode(ref) as HTMLCanvasElement;
+  };
+
   render() {
-    const { editorState } = this.state;
+    const { editorState, editorDimensions } = this.state;
     const { customFonts, target, defaultValue } = this.props;
     return (
       <div className="dce-container">
@@ -45,7 +69,7 @@ export class DraftCanvas extends Component<Props, State> {
           editorState={editorState}
           onChange={this.onChange}
         />
-        <Canvas editorState={editorState} canvas={target} />
+        <canvas style={{ display: "none" }} ref={this.getCanvasRef} />
         <button onClick={this.onSave} className="dce-save-btn">
           Save
         </button>
@@ -53,3 +77,5 @@ export class DraftCanvas extends Component<Props, State> {
     );
   }
 }
+
+// todo: init state
